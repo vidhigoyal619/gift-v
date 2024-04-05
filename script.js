f8a536bafa6f77fa0ca5a85f639ee36a4e9a8e9f
vidhigoyal619 <126322705+vidhigoyal619@users.noreply.github.com>
2024-04-05 21:14:46 +0530

    added files

---
+++ script.js
@@ -0,0 +1,21 @@
+function redirectToPage() {
+    // Change 'page.html' to the URL of the page you want to redirect to
+    window.location.href = 'slideshow.html';
+  }
+
+
+//   js for slidshow of images
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
